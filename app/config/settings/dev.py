e18688c1641e8267e73c1e847ed1b8faf6a4dd4c
sungmin414 <sungmin3231@gmail.com>
2019-05-20 19:37:11 +0900

    settings.dev, production의 DATABASES설정 변경

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -1,5 +1,5 @@
 from .base import *
-
+secrets = json.load(open(os.path.join(SECRETS_DIR, 'dev.json')))
 DEBUG = True
 
 WSGI_APPLICATION = 'config.wsgi.application'
--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,12 +1,8 @@
 from .base import *
+secrets = json.load(open(os.path.join(SECRETS_DIR, 'production.json')))
 
 DEBUG = False
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = secrets['DATABASES']
