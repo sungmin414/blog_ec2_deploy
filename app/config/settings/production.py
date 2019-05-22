e02aed7d4315165b9742e6fe2400815acb967851
sungmin414 <sungmin3231@gmail.com>
2019-05-22 18:41:16 +0900

    requirements구조 변경

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -8,6 +8,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = secrets['DATABASES']
 
 DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
+
 AWS_ACCESS_KEY_ID = secrets["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
